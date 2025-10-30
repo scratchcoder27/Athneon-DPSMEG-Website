e1a8973a1363f973e3e076857ea77c4f3cbd0036
ScratchCoder027 <debarpanmukherjee27@gmail.com>
2025-10-30 14:28:07 +0530

    Backend Changes - Partial

--- flaskmaps/maps.py
+++ flaskmaps/maps.py
@@ -10,4 +10,18 @@ bp = Blueprint('maps', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index/index.html')
+    
+    if (g.user):
+        db = get_db()
+        locations = db.execute(
+            '''
+            SELECT l.id, l.name, l.location_x, l.location_y, l.author_id, u.username
+            FROM location l
+            JOIN user u ON l.author_id = u.id
+            ORDER BY l.id DESC
+            '''
+        ).fetchall()
+    else:
+        locations = []
+
+    return render_template('index/index.html', locations=locations)
